80e338d57e274c674339461bed41d9e0c90973bd
GoncalvesF2 <filipe.goncalves@ppb.com>
2025-03-28 16:19:54 +0000

    Add module

--- SimpleActors/src/main/java/com/filipe/App.java
+++ SimpleActors/src/main/java/com/filipe/App.java
@@ -1,5 +1,9 @@
 package com.filipe;
 
+/**
+ * Hello world!
+ *
+ */
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
--- SimpleActors/src/test/java/com/filipe/AppTest.java
+++ SimpleActors/src/test/java/com/filipe/AppTest.java
@@ -5,15 +5,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-    /**
-     * Simple test example using JUnit Jupiter
-     */
+
     @Test
     public void testApp() {
         assertTrue(true, "Application should pass this test");
     }
 }
+
