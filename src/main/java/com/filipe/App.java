bb265dd586d5ecbc7c0e992fc950f19f5aac90ce
GoncalvesF2 <filipe.goncalves@ppb.com>
2025-03-28 16:07:09 +0000

    init commit

---
+++ src/main/java/com/filipe/App.java
@@ -0,0 +1,7 @@
+package com.filipe;
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/filipe/AppTest.java
@@ -0,0 +1,19 @@
+package com.filipe;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Simple test example using JUnit Jupiter
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true, "Application should pass this test");
+    }
+}
